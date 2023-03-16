21072a61271ea92663340e6ed0e46bd101de8ff0
bhumikacr <bhumikacr9@gmail.com>
2023-03-16 19:13:46 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def MB(request):
+    return render(request,'MB.html')
+
+def RC(request):
+    return render(request,'RC.html')
+
+def PK(request):
+    return render(request,'PK.html')
+
+def NTR(request):
+    return render(request,'NTR.html')
